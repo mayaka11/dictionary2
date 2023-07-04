93898f4a319a263e78698dcae2a84a2adfefa1f9
EC2 Default User <mayoneko0@gmail.com>
2023-07-04 12:31:17 +0000

    6章end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller? #devise利用の機能（ユーザ登録、ログイン認証など）が使われる前にconfigure_permitted_parametersメソッドが実行される
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])  #configure_permitted_parametersメソッドでは、devise_parameter_sanitizer.permitメソッドを使うことでユーザ登録の際にユーザ名のデータ操作を許可しています
+  end
+
+
 end
