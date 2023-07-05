562f6d2cc74096f2fa72dc7ee40170b466717c97
EC2 Default User <mayoneko0@gmail.com>
2023-07-05 14:34:32 +0000

    8章end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
+#ここで記入したことは作成しているアプリケーションすべてに影響する
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller? #devise利用の機能（ユーザ登録、ログイン認証など）が使われる前にconfigure_permitted_parametersメソッドが実行される
+
+
+  def after_sign_in_path_for(resource)  #サインイン後の遷移先の設定をするメソッド
+    about_path  #Deviseの初期設定ではroot_pathになっていますが、この記入によりAboutページへ遷移するように設定されました。
+  end
+
+  def after_sign_out_path_for(resource)  #サインアウト後の遷移先の設定をするメソッド
+    about_path
+  end
+
+
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  get 'homes/about' => 'homes#about', as: 'about' #名前付きルート
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
