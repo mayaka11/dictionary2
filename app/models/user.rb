6a06d1e596da8880ef6c5e2410f93e0e405902db
EC2 Default User <mayoneko0@gmail.com>
2023-07-09 09:42:46 +0000

    16章完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,16 @@ class User < ApplicationRecord
 
   has_many :post_images,dependent: :destroy  #UserモデルにPostImageモデルを関連付ける。Userモデルに対してPostImageモデルが1:Nになるように関連付ける
   #has_manyは直訳すると「たくさん持っている」なので、1:Nの「1」側に当たるモデルに、has_manyを記載する必要がある
+
+
+  has_one_attached :profile_image #profile_imageという名でActiveStorageでプロフィール画像を保存できるように設定した。
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed  #画像サイズの変更。
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,14 @@ Rails.application.routes.draw do
   #get 'post_images/index'
   #get 'post_images/show'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] #上記3つをまとめて書いた形
+
+  #get 'users/show'
+  #get 'users/edit'
+  resources :users, only: [:show, :edit, :update] #上記2つをまとめて書いた形
+
+
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
